22b7f1db95e7648f8fe03bacd06b2c54ce92ee6f
Daniel Keen <keendaniel01@gmail.com>
2018-01-29 15:50:50 +0000

    challenge #08 completed: add touch in and out functionality

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -4,10 +4,27 @@ class Oystercard
 
   def initialize
     @balance = 0
+    @in_journey = false
   end
 
   def top_up(amount)
     raise "That top up would take you over your card limit of #{CARD_LIMIT}" if @balance + amount > CARD_LIMIT
     @balance += amount
+  end
+
+  def deduct(amount)
+    @balance -= amount
+  end
+
+  def touch_in
+    @in_journey = true
+  end
+
+  def touch_out
+    @in_journey = false
+  end
+
+  def in_journey?
+    @in_journey
   end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -11,7 +11,36 @@ describe Oystercard do
     end
 
     it "should not top up over the card limit" do
-      expect { subject.top_up 91 }.to raise_error "That top up would take you over your card limit of #{described_class::CARD_LIMIT}"
+      card_limit = described_class::CARD_LIMIT
+      subject.top_up(card_limit)
+      expect { subject.top_up 1 }.to raise_error "That top up would take you over your card limit of #{card_limit}"
+    end
+  end
+
+  describe "#deduct" do
+    it "should deduct the balance by specified amount" do
+      expect { subject.deduct 20 }.to change { subject.balance }.by -20
+    end
+  end
+
+  describe "#in_journey?" do
+    it "outputs a true/false return value" do
+      expect(subject).not_to be_in_journey
+    end
+  end
+
+  describe "#touch_in" do
+    it "should change in_journey? to true" do
+      subject.touch_in
+      expect(subject).to be_in_journey
+    end
+  end
+
+  describe "#touch_out" do
+    it "should change in_journey? to false" do
+      subject.touch_in
+      subject.touch_out
+      expect(subject).to_not be_in_journey
     end
   end
 end
