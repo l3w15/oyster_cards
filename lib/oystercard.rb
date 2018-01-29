39f31e52ece4b14445d87b01f6362e2cd6fb384f
Lewis Watson <lewwwiswatson@gmail.com>
2018-01-29 15:27:18 +0000

    complete challenge #06: add card limit

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,7 +1,13 @@
 class Oystercard
   attr_reader :balance
+  CARD_LIMIT = 90
 
   def initialize
     @balance = 0
+  end
+
+  def top_up(amount)
+    raise "That top up would take you over your card limit of #{CARD_LIMIT}" if @balance + amount > CARD_LIMIT
+    @balance += amount
   end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,4 +4,14 @@ describe Oystercard do
   it "should have a balance" do
     expect(subject.balance).to eq 0
   end
+
+  describe "#top_up" do
+    it "should top up the balance by a specified amount" do
+      expect { subject.top_up 20 }.to change { subject.balance }.by 20
+    end
+
+    it "should not top up over the card limit" do
+      expect { subject.top_up 91 }.to raise_error "That top up would take you over your card limit of #{described_class::CARD_LIMIT}"
+    end
+  end
 end
